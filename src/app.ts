36664f9ef0c13f946d1639b5e7bc698a46e6e2a0
rafael veiga <rafaelveig@hotmail.com>
2022-02-23 15:58:54 -0300

    validacoes de nome e password

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import createUser from './Controller/UserController';
-import validadeUser from './middlewares/uservalidation';
+import {
+  nameValidation,
+  classValidation,
+  levelValidation,
+  passwordValidation,
+} from './middlewares/uservalidation';
 
 const app = express();
 
 app.use(express.json());
-app.post('/users', validadeUser, createUser);
+app.post(
+  '/users',
+  nameValidation,
+  classValidation,
+  levelValidation,
+  passwordValidation,
+  createUser,
+);
 
 export default app;
--- src/middlewares/uservalidation.ts
+++ src/middlewares/uservalidation.ts
@@ -40,3 +40,50 @@ const levelValidation = (req: Request, res: Response, next: NextFunction) => {
   }
   next();
 };
+
+const nameValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { username } = req.body;
+
+  if (username === undefined) {
+    return res.status(StatusCode.BAD_REQUEST).json({
+      error: 'Username is required' });
+  }
+
+  if (typeof username !== 'string') {
+    return res.status(StatusCode.UNPROCCESABLE_ENTITY).json({
+      error: 'Username must be a string' });
+  }
+
+  if (username.length <= 2) {
+    return res.status(StatusCode.UNPROCCESABLE_ENTITY).json({
+      error: 'Username must be at least 2 characters' });
+  }
+  next();
+};
+
+const passwordValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+
+  if (password === undefined) {
+    return res.status(StatusCode.BAD_REQUEST).json({
+      error: 'Password is required' });
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(StatusCode.UNPROCCESABLE_ENTITY).json({
+      error: 'Password must be a string' });
+  }
+
+  if (password.length < 8) {
+    return res.status(StatusCode.UNPROCCESABLE_ENTITY).json({
+      error: 'Password must be at least 7 characters' });
+  }
+  next();
+};
+
+export default {
+  nameValidation,
+  classValidation,
+  levelValidation,
+  passwordValidation,
+};
