41bc1d5bad83fc7776e8e640f7ad291398cff553
rafael veiga <rafaelveig@hotmail.com>
2022-02-23 20:00:40 -0300

    teste login

--- src/Controller/Login.ts
+++ src/Controller/Login.ts
@@ -3,19 +3,18 @@ import { UserSemID } from '../interfaces/User';
 import UserModel from '../models/UserModel';
 
 const userLogin = async (user: UserSemID) => {
-  try {
-    const userFound: UserSemID = await UserModel.getUserByUsername(user);
-    if (!userFound) {
-      return res
-        .status(StatusCode.NOT_FOUND)
-        .message('Username or password not found');
-    } 
-    return userFound;
-  } catch (err) {
-    return res
-      .status(StatusCode.BAD_REQUEST)
-      .message('Username or password not found');
+  const userFound = await UserModel.getUserByUsername(user);
+  if (!userFound) {
+    return {
+      status: StatusCode.UNAUTHORIZED,
+      message: 'Username or password invalid',
+    };
   }
+  return {
+    status: StatusCode.OK,
+    message: 'Login successful',
+    user: userFound,
+  };
 };
 
 export default userLogin;
